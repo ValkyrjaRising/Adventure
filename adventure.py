ad76a6ed35c6658bd12910663d53942ffe546656
ValkyrjaRising <74553178+ValkyrjaRising@users.noreply.github.com>
2024-08-06 12:59:03 -0400

    migration

--- adventure.py
+++ adventure.py
@@ -18,13 +18,15 @@ def main():
     hello('Welcome adventurer! Please, introduce yourself and select your path to victory!')
     name = name_select()
     character = character_select(name)
-
+    battle_continue = True
     score = 0
     while True:
         enemy = generate_enemy()
-        score, player_health, enemy = battlestate(score, character, enemy)
+        score, player_health, enemy, battle_continue = battlestate(score, character, enemy, battle_continue)
         #write_score(score, name)
-
+        if battle_continue != True:
+            break
+    print('You got out of battle!')
 
 if __name__ == "__main__":
     main()
--- battle_system.py
+++ battle_system.py
@@ -5,13 +5,13 @@ from enemies import enemy_select, enemy_score_values
 import character_info
 
 
-def battlestate(score, character, enemy):
+def battlestate(score, character, enemy, battle_continue):
     print(f'A wild {enemy.name} has appeared!')
 
     while enemy.health > 0:
         print('Choose your action!')
         try:
-            choice = int(input('1. Attack \n2. Magic \n3. Run Away! \n'))
+            choice = int(input('1. Attack \n2. Magic \n3. Item \n4. Use Skill \n5. Run Away! \n'))
         except ValueError:
             print('Please enter a valid choice!')
             continue
@@ -23,6 +23,14 @@ def battlestate(score, character, enemy):
                 enemy.health -= attack
             else:
                 print(f'You try to hit {enemy.name} but miss!')
+        elif choice == 2:
+            print(f'You cast a spell at {enemy.name}')
+        elif choice == 3:
+            print(f'You use an item. #enter item name and effect here')
+        elif choice == 4:
+            print(f'You use your skill. #enter skill name and effect here')
+        elif choice == 5:
+            print(f'You run away from {enemy.name}')
 
         if enemy.health > 0:
             if random.randint(1, 10) > 3:
@@ -36,7 +44,17 @@ def battlestate(score, character, enemy):
         else:
             print(f'You have slain the {enemy.name}!!!')
             score += enemy_score_values.get(enemy.name, 0)
-            print(f'You\'re score is currently {score}')
             print('The enemy dropped some loot!')
+            print(f'You\'re score is currently {score}')
             enemy = enemies.generate_enemy()
-            return score, character, enemy
+            while True:
+                new_challenge = input("Do you want to continue fighting? \n(Yes/No): ").strip()
+                if new_challenge.capitalize() in ['Yes', 'Y']:
+                    battle_continue = True
+                    break
+                elif new_challenge.capitalize() in ['No', 'N']:
+                    battle_continue = False
+                    break
+                else:
+                    print('Please enter a valid option')
+            return score, character, enemy, battle_continue
